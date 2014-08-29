f66fbfee3b95fc6738f24b88db1fca67ee7abcca
Kai <kai.bepperling@gmail.com>
2014-08-29 09:19:30 +0200

    config fix

--- test.rb
+++ test.rb
@@ -1,16 +1,24 @@
 require 'rubygems'
 require 'trollop'
+require	'json'
+require	'zip'
 
-opts = Trollop::options do
-  opt :no, "BEST_COMPRESSION"
-  opt :best, "NO_COMPRESSION"
+config = JSON.parse(File.read 'config.json')
 
+Zip::File.open(config["zipfile_name"]) do |zip_file|
+  # Handle entries one by one
+  zip_file.each do |entry|
+    # Extract to file/directory/symlink
+    puts "Extracting #{entry.name}"
+    entry.extract(dest_file)
+
+    # Read into memory
+    content = entry.get_input_stream.read
+  end
+
+  # Find specific entry
+  #entry = zip_file.glob('*.csv').first
+  #puts entry.get_input_stream.read
 end
-
-if opts[:no] == true
-        x="NO_COMPRESSION"
-        puts x
-        
-    end 
 
 
